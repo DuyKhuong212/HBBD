30bb50c427e61ec956686bfdf54fae299cd6f561
DuyKhuong212 <nguyenduykhuong0201t@gmail.com>
2023-06-02 12:41:03 +0700

    firt commit hbbd

---
+++ index.js
@@ -0,0 +1,106 @@
+const contentLetterSrart_actived = "Chúc mày sinh nhật vui vẻ, tuổi mới xinh lại thêm xinh, duyên lại càng thêm duyên và yêu đời nhé!" //Lời mở đầu cho bức thư
+const mainContentLetter = "Happy birthday bạn yêu, biết bạn không thích gì ngoài tiền nên nhân dịp sinh nhật, chúc bạn tôi công thành danh toại, tiền vào như nước để còn thường xuyên dắt mình đi ăn ké. " 
+
+let imgStart = document.querySelector(".myAI"); 
+imgStart.src = "./img/cute-young-boy-kid-wearing-vest-and-hat-free-png.png";
+
+let imgLetter = document.querySelector(".img");
+imgLetter.src = "./img/cute-young-boy-kid-wearing-vest-and-hat-free-png.png"; 
+
+const splitContentLetterSrart_actived = contentLetterSrart_actived.split("");
+
+document.querySelector(".sticker").addEventListener("click", function () {
+    document.querySelector(".contentLetter").innerHTML = "";
+    document.querySelector(".startLetter").classList.add("active")
+    setTimeout(() => {
+        splitContentLetterSrart_actived.forEach((val, index) => {
+            setTimeout(() => {
+                document.querySelector(".contentLetter").innerHTML += val;
+                if (index == contentLetterSrart_actived.length - 1) {
+                    setTimeout(() => {
+                        document.querySelector(".recieve").setAttribute("style", "opacity: 1; transition: .5s") 
+                    }, 1000)
+                }
+            }, 50 * index)
+        })
+    }, 1000)
+})
+
+document.querySelector("#mess").addEventListener("change", function () {
+    if (this.checked == true) {
+        document.querySelector(".content").classList.add("actived");
+        const splitMainContentLetter = mainContentLetter.split("");
+
+        splitMainContentLetter.forEach((val, index) => {
+            setTimeout(() => {
+                document.querySelector(".mainContent").innerHTML += val;
+                if (index == mainContentLetter.length - 1) {
+                    document.querySelector(".img1").setAttribute("style", "opacity: 1; transition: .5s")
+                }
+            }, 50 * index)
+        })
+
+    } else {
+        document.querySelector(".content").classList.remove("actived")
+        document.querySelector(".img1").setAttribute("style", "opacity: 0; transition: .5s")
+        document.querySelector(".mainContent").innerHTML = "";
+    }
+})
+
+document.querySelector(".recieve").addEventListener("click", () => {
+    document.querySelector(".startLetter").classList.add("close");
+    setTimeout(() => {
+        document.querySelector(".startForm").classList.add("close");
+        setTimeout(() => {
+            document.querySelector(".startForm").setAttribute("style", "bottom: 100%");
+            
+            let getTypeDevice = document.documentElement.clientWidth;
+            if (getTypeDevice <= 768) {
+                createLight(20)
+            } else {
+                createLight(40)
+            }
+
+        }, 500)
+    }, 500)
+})
+
+const getBackground = document.querySelector(".backgroundParty");
+var width = getBackground.offsetWidth;
+var height = getBackground.offsetHeight;
+
+function createLight(a) {
+    var container = document.querySelector(".backgroundParty");
+    const blurLv = [2, 4];
+    const count = a;
+    const allDefaultColor = ["red", "lime", "yellow", "orange", "blue"]
+
+    for (var i = 0; i < count; i++) {
+        var randomLeft = 0;
+        randomLeft = Math.floor(Math.random() * width);
+        var randomTop = 0;
+        randomTop = Math.floor(Math.random() * height / 2);
+        var color = "white";
+        var blur = Math.floor(Math.random() * 2);
+        var widthEle = Math.floor(Math.random() * 5) + 15;
+        var moveTime = Math.floor(Math.random() * 4) + 4;
+
+        var div = document.createElement("div");
+        div.classList.add = "snow";
+        div.style.position = "absolute";
+        div.style.backgroundColor = allDefaultColor[Math.floor(Math.random() * 5)]
+        div.style.borderRadius = Math.floor(Math.random() * 10 + 10).toString() + "px"
+
+        div.style.height = "0px";
+        div.style.width = "0px";
+
+        div.style.height = widthEle * Math.floor(Math.random() * 4 + 1) + "px";
+        div.style.width = widthEle + "px";
+        div.style.marginLeft = randomLeft + "px"
+        div.style.marginTop = randomTop + "px"
+        div.style.filter = "blur(" + blurLv[blur] + "px" + ")"
+        div.style.animation = "moveLight " + moveTime + "s ease-in-out infinite";
+
+        container.appendChild(div);
+    }
+}
